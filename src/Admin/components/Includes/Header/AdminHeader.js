c2d44df08b71bba419590ab322cb7309f8666c29
devrsingh2 <rsingh2@katalysttech.com>
2019-12-27 19:05:19 +0530

    working on admin panel

--- src/Admin/components/Includes/Header/AdminHeader.js
+++ src/Admin/components/Includes/Header/AdminHeader.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import classNames from 'classnames';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import SearchIcon from '@material-ui/icons/Search';
-import IconButton from '@material-ui/core/IconButton';
+import {Button, IconButton, Menu, MenuItem} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
-import Hidden from '@material-ui/core/Hidden';
-import styles from './styles';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import SaveIcon from '@material-ui/icons/Save';
+import EditIcon from '@material-ui/icons/Edit';
 import Typography from "@material-ui/core/Typography";
 import {makeStyles} from "@material-ui/core";
+import {Link} from "react-router-dom";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import './index.scss';
 
 const useStyles = makeStyles(theme => ({
     appBar: {
@@ -20,28 +21,229 @@ const useStyles = makeStyles(theme => ({
     },
     menuButton: {
         marginLeft: 12,
-        marginRight: 20
+        // marginRight: 20
+        marginRight: 0
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    sectionDesktop: {
+        display: 'none',
+        [theme.breakpoints.up('md')]: {
+            display: 'flex',
+        },
+    },
+    sectionMobile: {
+        display: 'flex',
+        [theme.breakpoints.up('md')]: {
+            display: 'none',
+        },
     },
 }));
 
 const AdminHeader = (props) => {
     const classes = useStyles();
-    return (
-        <AppBar className={classes.appBar} elevation={0}>
-            <Toolbar disableGutters={true}>
-                <IconButton
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const handleProfileMenuOpen = event => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMobileMenuClose = () => {
+        setMobileMoreAnchorEl(null);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+        handleMobileMenuClose();
+    };
+
+    const handleMobileMenuOpen = event => {
+        setMobileMoreAnchorEl(event.currentTarget);
+    };
+
+    const menuId = 'primary-search-account-menu';
+    const renderMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+        >
+            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+            <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+        </Menu>
+    );
+
+    const mobileMenuId = 'primary-search-account-menu-mobile';
+    const renderMobileMenu = (
+        <Menu
+            anchorEl={mobileMoreAnchorEl}
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            id={mobileMenuId}
+            keepMounted
+            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            open={isMobileMenuOpen}
+            onClose={handleMobileMenuClose}
+        >
+            <MenuItem>
+                <Button
+                    component={Link}
+                    className={classes.button}
+                    raised
                     color="inherit"
-                    aria-label="Open drawer"
-                    onClick={props.handleDrawerToggle}
-                    className={classes.menuButton}
+                    to="/"
                 >
-                    <MenuIcon />
+                    {'Home'}
+                </Button>
+            </MenuItem>
+            <MenuItem>
+                <Button
+                    component={Link}
+                    className={classes.button}
+                    raised
+                    color="inherit"
+                    to="/about"
+                >
+                    {'About'}
+                </Button>
+            </MenuItem>
+            <MenuItem>
+                <Button
+                    component={Link}
+                    className={classes.button}
+                    raised
+                    color="inherit"
+                    to="/login"
+                >
+                    {'Login'}
+                </Button>
+            </MenuItem>
+            <MenuItem>
+                <Button
+                    component={Link}
+                    className={classes.button}
+                    raised
+                    color="inherit"
+                    to="/register"
+                >
+                    {'Register'}
+                </Button>
+            </MenuItem>
+            <MenuItem onClick={handleProfileMenuOpen}>
+                <IconButton
+                    aria-label="account of current user"
+                    aria-controls="primary-search-account-menu"
+                    aria-haspopup="true"
+                    color="inherit"
+                >
+                    <AccountCircle />
                 </IconButton>
-                <Typography variant="title" color="inherit" noWrap>
-                    {process.env.REACT_APP_NAME || 'ReactJS ADMIN'}
-                </Typography>
-            </Toolbar>
-        </AppBar>
+                <p>Profile</p>
+            </MenuItem>
+        </Menu>
+    );
+
+    return (
+        <div className={classes.grow}>
+            <AppBar className={classes.appBar} elevation={0}>
+                <Toolbar disableGutters={true}>
+                    <IconButton
+                        color="inherit"
+                        aria-label="Open drawer"
+                        onClick={props.handleDrawerToggle}
+                        className={classes.menuButton}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="title" color="inherit" noWrap>
+                        {process.env.REACT_APP_NAME || 'ReactJS ADMIN'}
+                    </Typography>
+
+                    <div className={classes.grow} />
+                    <div className={classes.sectionDesktop}>
+                        {/*<IconButton aria-label="show 4 new mails" color="inherit">
+                            <Badge badgeContent={4} color="secondary">
+                                <MailIcon />
+                            </Badge>
+                        </IconButton>
+                        <IconButton aria-label="show 17 new notifications" color="inherit">
+                            <Badge badgeContent={17} color="secondary">
+                                <NotificationsIcon />
+                            </Badge>
+                        </IconButton>*/}
+
+                        <Button
+                            component={Link}
+                            className={classes.button}
+                            raised
+                            color="inherit"
+                            to="/"
+                        >
+                            {'Home'}
+                        </Button>
+                        <Button
+                            component={Link}
+                            className={classes.button}
+                            raised
+                            color="inherit"
+                            to="/about"
+                        >
+                            {'About'}
+                        </Button>
+                        <Button
+                            component={Link}
+                            className={classes.button}
+                            raised
+                            color="inherit"
+                            to="/login"
+                        >
+                            {'Login'}
+                        </Button>
+                        <Button
+                            component={Link}
+                            className={classes.button}
+                            raised
+                            color="inherit"
+                            to="/register"
+                        >
+                            {'Register'}
+                        </Button>
+                        <IconButton
+                            edge="end"
+                            aria-label="account of current user"
+                            aria-controls={menuId}
+                            aria-haspopup="true"
+                            onClick={handleProfileMenuOpen}
+                            color="inherit"
+                        >
+                            <AccountCircle />
+                        </IconButton>
+                    </div>
+                    <div className={classes.sectionMobile}>
+                        <IconButton
+                            aria-label="show more"
+                            aria-controls={mobileMenuId}
+                            aria-haspopup="true"
+                            onClick={handleMobileMenuOpen}
+                            color="inherit"
+                        >
+                            <MoreIcon />
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+            {renderMobileMenu}
+            {renderMenu}
+        </div>
     )
 };
 
